b922e35712801ffd0ad5fea0d606f620b157e346
shivanshtripathi707 <webgen966@gmail.com>
2024-02-14 21:50:39 +0530

    Website Update

---
+++ index.js
@@ -0,0 +1,74 @@
+// ----------------------------Game Sound--------------------------------
+
+var audioplay = document.getElementById("audioplay");
+var audio1 = new Audio("Game Bg Tone.mp3");
+
+audioplay.addEventListener("click",function(){
+     if(audioplay.classList.contains("fa-volume-xmark")){
+         audioplay.classList.remove("fa-volume-xmark");
+         audioplay.classList.add("fa-volume-high");
+         audio1.play();
+     }
+     else{
+        audio1.pause();
+        audioplay.classList.remove("fa-volume-high");
+        audioplay.classList.add("fa-volume-xmark");
+     }
+})
+
+// ----------------------------------Game Logic----------------------------------
+
+var score = 0;
+var RandowNumberGen = Math.floor(Math.random()*100);
+
+function makeBubble(){
+    var bubbleCreate = "";
+    var page = document.querySelector(".mid_main_page");
+    for(var i=0; i<=329; i++){
+    var Numgen = Math.floor(Math.random()*100);
+    bubbleCreate += `<div class="bubble">${Numgen}</div>`;
+}
+    page.innerHTML = bubbleCreate;
+}
+
+function randomHit(){
+    var hitbox = document.getElementById("Hit");
+        RandowNumberGen = Math.floor(Math.random()*100);
+        hitbox.innerHTML = RandowNumberGen;
+}
+
+function runTime(){
+    let timer = 60;
+    var timerbox = document.getElementById("Time");
+    const RunningTime = setInterval(function(){
+        if(timer > 0){
+            timer--;
+            timerbox.textContent = timer;
+        }
+        else{
+            clearInterval(RunningTime);
+            document.querySelector(".mid_main_page").innerHTML = `<h1 id="Gamerover">Game Over`;
+            document.getElementById("Hit").innerHTML = 0;
+        }
+    },1000)
+}
+
+function IncreaseScore(){
+    var scorebox = document.getElementById("Score");
+    score += 10;
+    scorebox.textContent = score;
+}
+
+document.querySelector(".mid_main_page").addEventListener("click",function(dets){
+    var clickedNumber = Number(dets.target.textContent);
+    if(clickedNumber === RandowNumberGen){
+        IncreaseScore();
+        randomHit();
+        makeBubble();
+    }
+})
+
+
+makeBubble();
+randomHit();
+runTime();
